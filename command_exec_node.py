0716a53c6079054e0f6c4c2590e1cd92762ba569
Jean Kássio <jeankassiocheib@gmail.com>
2025-06-11 21:24:38 -0300

    bugfix in input type

--- command_exec_node.py
+++ command_exec_node.py
@@ -1,17 +1,21 @@
 import subprocess
 
+class AnyType(str):
+    def __ne__(self, __value: object) -> bool:
+        return False
+
+any_typ = AnyType("*")
+
 class CommandExecNode:
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {
+                "input": (any_typ,),
                 "command": ("STRING", {
                     "multiline": True,
                     "default": "echo Hello World"
                 }),
-            },
-            "optional": {
-                "input": ("*", "*")
             },
         }
 
@@ -20,16 +24,16 @@ class CommandExecNode:
     FUNCTION = "run_command"
     CATEGORY = "Custom/Utility"
 
-    def run_command(self, command):
+    def run_command(self, input, command):
+        import subprocess
         try:
-            # Executa o comando no shell
             result = subprocess.check_output(
                 command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True
             )
         except subprocess.CalledProcessError as e:
             result = f"[ERROR] Code {e.returncode}:\n{e.output}"
-
         return (result,)
+
 
 # Registro do node no ComfyUI
 NODE_CLASS_MAPPINGS = {
